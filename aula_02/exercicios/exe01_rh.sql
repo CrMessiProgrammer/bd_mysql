33361b8998a02c34465f764fd81a99a487f51d0c
Carlos Henrique Nunes <carloshnunes383@gmail.com>
2025-01-08 02:35:26 -0300

    :sparkles: Update - Exercícios Propostos

--- aula_02/exercicios/exe01_rh.sql
+++ aula_02/exercicios/exe01_rh.sql
@@ -1,7 +1,10 @@
+-- Criar Banco de Dados
 CREATE DATABASE db_rh;
 
+-- Acessar (Selecionar) o Banco de Dados
 USE db_rh;
 
+-- Criar Tabela
 CREATE TABLE tb_colaboradores(
 	id BIGINT AUTO_INCREMENT,
     nome VARCHAR(100) NOT NULL,
@@ -14,30 +17,41 @@ CREATE TABLE tb_colaboradores(
     PRIMARY KEY(id)
 );
 
+-- Inserir dados na tabela
 INSERT INTO tb_colaboradores (nome, data_admissao, email, cargo, departamento, salario, status)
 VALUES ("Carlos Henrique", "2024-01-10", "carlos@gmail.com", "Desenvolvedor Back End", "Tecnologia da Informação (TI)", 2000.00, "ativo");
 
+-- Inserir mais dados na tabela
 INSERT INTO tb_colaboradores (nome, data_admissao, email, cargo, departamento, salario, status)
 VALUES ("Marcelo Nunes", "2014-10-10", "marcelon@gmail.com", "Analista de Marketing", "Marketing", 5000.00, "ativo"),
-("Marcelo Henrique", "2024-10-12", "marceloh@gmail.com", "Desenvolvedor Front End", "Tecnologia da Informação (TI)", 1800.00, "inativo"),
-("Rafael Queiroz", "2010-01-05", "rafaelq@gmail.com", "Desenvolvedor Full Stack", "Tecnologia da Informação (TI)", 10000.00, "ativo"),
-("Aimee Thompson", "2012-07-12", "aimeet@gmail.com", "Desenvolvedora Full Stack", "Tecnologia da Informação (TI)", 10000.00, "ativo"),
-("Daniel Mendes", "2018-01-10", "danielm@gmail.com", "Gerente de Recursos Humanos", "Recursos Humanos (RH)", 6000.00, "inativo"),
-("Tatiana Ferreira", "2022-08-05", "tatiana@gmail.com", "Assistente Administrativo", "Administração", 3200.00, "ativo"),
-("Marilene da Silva", "2022-02-11", "marilenes@gmail.com", "Gerente de Recursos Humanos", "Recursos Humanos (RH)", 6500.00, "ativo"),
-("Natalia Tavares", "2023-05-05", "nataliat@gmail.com", "Consultora de Vendas", "Comercial", 4200.00, "ativo");
+	   ("Marcelo Henrique", "2024-10-12", "marceloh@gmail.com", "Desenvolvedor Front End", "Tecnologia da Informação (TI)", 1800.00, "inativo"),
+	   ("Rafael Queiroz", "2010-01-05", "rafaelq@gmail.com", "Desenvolvedor Full Stack", "Tecnologia da Informação (TI)", 10000.00, "ativo"),
+	   ("Aimee Thompson", "2012-07-12", "aimeet@gmail.com", "Desenvolvedora Full Stack", "Tecnologia da Informação (TI)", 10000.00, "ativo"),
+	   ("Daniel Mendes", "2018-01-10", "danielm@gmail.com", "Gerente de Recursos Humanos", "Recursos Humanos (RH)", 6000.00, "inativo"),
+	   ("Tatiana Ferreira", "2022-08-05", "tatiana@gmail.com", "Assistente Administrativo", "Administração", 3200.00, "ativo"),
+	   ("Marilene da Silva", "2022-02-11", "marilenes@gmail.com", "Gerente de Recursos Humanos", "Recursos Humanos (RH)", 6500.00, "ativo"),
+	   ("Natalia Tavares", "2023-05-05", "nataliat@gmail.com", "Consultora de Vendas", "Comercial", 4200.00, "ativo");
 
-SELECT id, nome, email, cargo, departamento, salario, status,
-date_format(data_admissao, '%d/%m/%Y') AS Data_Admissao
+-- Visualizar todos os dados
+SELECT id, nome, email, cargo, departamento, status,
+date_format(data_admissao, '%d/%m/%Y') AS Data_Admissao,
+concat('R$ ', FORMAT(salario, 2, 'pt-BR')) AS Salario
 FROM tb_colaboradores;
 
-SELECT id, nome, email, cargo, departamento, salario, status,
-date_format(data_admissao, '%d/%m/%Y') AS Data_Admissao
+-- Visualizar somente os colaboradores com salário maior que 2000
+SELECT id, nome, email, cargo, departamento, status,
+date_format(data_admissao, '%d/%m/%Y') AS Data_Admissao,
+concat('R$ ', FORMAT(salario, 2, 'pt-BR')) AS Salario
 FROM tb_colaboradores WHERE salario > 2000;
 
-SELECT id, nome, email, cargo, departamento, salario, status,
-date_format(data_admissao, '%d/%m/%Y') AS Data_Admissao
+-- Visualizar somente os colaboradores com salário menor que 2000
+SELECT id, nome, email, cargo, departamento, status,
+date_format(data_admissao, '%d/%m/%Y') AS Data_Admissao,
+concat('R$ ', FORMAT(salario, 2, 'pt-BR')) AS Salario
 FROM tb_colaboradores WHERE salario < 2000;
 
+-- Atualizar o atributo salario, onde id for igual a 4
 UPDATE tb_colaboradores SET salario = 12000.00 WHERE id = 4;
+
+-- Atualizar o atributo status, onde id for igual a 9
 UPDATE tb_colaboradores SET status = "inativo" WHERE id = 9;
--- aula_02/exercicios/exe02_loja.sql
+++ aula_02/exercicios/exe02_loja.sql
@@ -1,3 +1,53 @@
+-- Criar Banco de Dados
 CREATE DATABASE db_loja;
 
+-- Acessar (Selecionar) o Banco de Dados
 USE db_loja;
+
+-- Criar Tabela
+CREATE TABLE tb_produtos(
+	id BIGINT AUTO_INCREMENT,
+    nome VARCHAR(100) NOT NULL,
+    valor DECIMAL (8,2) NOT NULL,
+    estoque INT NOT NULL,
+    categoria VARCHAR(50),
+    marca VARCHAR(20),
+    descricao VARCHAR(255),
+    PRIMARY KEY(id)
+);
+
+-- Inserir dados na tabela
+INSERT INTO tb_produtos (nome, valor, estoque, categoria, marca, descricao)
+VALUES ("Notebook Gamer", 4199.90, 20, "Eletrônicos", "Acer", "Notebook para alto desempenho");
+
+-- Inserir mais dados na tabela
+INSERT INTO tb_produtos (nome, valor, estoque, categoria, marca, descricao)
+VALUES ("Notebook", 2199.99, 50, "Eletrônicos", "Samsung", "Notebook para uso casual"),
+	   ("Mouse", 149.90, 25, "Acessórios", "Logitech", "Mouse sem fio"),
+	   ("Monitor", 799.90, 15, "Eletrônicos", "Aoc", "Monitor Full HD 24 polegadas 120Hz"),
+	   ("Teclado", 490.99, 200, "Acessórios", "Redragon", "Teclado Mecânico Sem Fio"),
+	   ("Cadeira Gamer", 1199.90, 30, "Móveis", "ThunderX3", "Cadeira ergonômica para jogos"),
+	   ("Headset", 299.90, 80, "Acessórios", "HyperX", "Headset com som surround"),
+	   ("Impressora", 899.90, 15, "Eletrônicos", "HP", "Impressora multifuncional"),
+	   ("Memória RAM", 249.90, 100, "Eletrônicos", "Kingston", "Memória DDR4 de 16GB 3200MHz");
+
+-- Visualizar todos os dados
+SELECT id, nome, estoque, categoria, marca, descricao,
+concat('R$ ', FORMAT(valor, 2, 'pt-BR')) AS Valor
+FROM tb_produtos;
+
+-- Visualizar somente os produtos com valor maior que 500
+SELECT id, nome, estoque, categoria, marca, descricao,
+concat('R$ ', FORMAT(valor, 2, 'pt-BR')) AS Valor
+FROM tb_produtos WHERE valor > 500.00;
+
+-- Visualizar somente os produtos com valor menor que 500
+SELECT id, nome, estoque, categoria, marca, descricao,
+concat('R$ ', FORMAT(valor, 2, 'pt-BR')) AS Valor
+FROM tb_produtos WHERE valor < 500.00;
+
+-- Atualizar os atributos estoque e valor, onde id for igual a 5
+UPDATE tb_produtos SET estoque = 60, valor = 379.90 WHERE id = 5;
+
+-- Atualizar o atributo descricao, onde id for igual a 7
+UPDATE tb_produtos SET descricao = "Headset com som surround e Wireless de 2,4GHz" WHERE id = 7;
